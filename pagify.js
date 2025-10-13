19d040914891f5e31cf7913d6a698f7f73a8efa7
govind-eka <govind.ganeriwal@eka.care>
2025-10-13 18:40:11 +0530

    changed 0 to array of 0

--- pagify.js
+++ pagify.js
@@ -537,7 +537,7 @@ class PagifySDK {
                     console.log('Estimated pages:', totalPages);
                     
                     const opt = {
-                        margin: 0,
+                        margin: [0,0,0,0],
                         filename: "document.pdf",
                         image: { 
                             type: "jpeg", 
